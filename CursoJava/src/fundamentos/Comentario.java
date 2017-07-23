e2ee562817a41dd4bcc4a62534cf7b8e48a9b538
Edilson Borges <edilsonfb@gmail.com>
2017-07-22 23:57:49 -0300

    utilizando comentários de documentação

--- CursoJava/src/fundamentos/Comentario.java
+++ CursoJava/src/fundamentos/Comentario.java
@@ -1,10 +1,18 @@
 package fundamentos;
 
+/**
+ * Classe responsável por mostrar o uso dos 3 tipos de comentários que o Java suporta.
+ * @author Edilson [contato@edilsonborges.com.br]
+ * @version 1.0
+ * @since 2.2
+ *
+ */
 public class Comentario {
 
 	/**
+	 * Esse método é a porta de entrada pde um programa Java.
 	 * 
-	 * @param args
+	 * @param args parâmetro passados por linha de comando
 	 */
 	public static void main(String[] args){
 		/*
