c2ebab802631dab2fbcfd08b39e805d60c51054d
Edilson Borges <edilsonfb@gmail.com>
2017-07-23 23:19:21 -0300

    valores para variaveis e constantes

--- CursoJava/src/fundamentos/VariaveisEConstantes.java
+++ CursoJava/src/fundamentos/VariaveisEConstantes.java
@@ -3,6 +3,8 @@ package fundamentos;
 public class VariaveisEConstantes {
 
 	public static void main(String[] args){
-
+		double raio = 4.5;
+		final double PI = 3.1415;
+		double area = PI * raio * raio;
 	}
 }
