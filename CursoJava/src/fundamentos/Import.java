db2dce8aad6768668fa1f87d5020ac40b4db2465
Edilson Borges <edilsonfb@gmail.com>
2017-07-24 23:40:22 -0300

    importanto java.util.Date

--- CursoJava/src/fundamentos/Import.java
+++ CursoJava/src/fundamentos/Import.java
@@ -1,8 +1,12 @@
 package fundamentos;
 
+import java.sql.Date;
+
 public class Import {
 
 	public static void main(String[] args){
+		Double a = 2.0;
 		System.out.println("...");
+		Date d;
 	}
 }
