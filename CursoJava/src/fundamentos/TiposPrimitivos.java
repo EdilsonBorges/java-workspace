a4c68401177f416a8e04596417bee8daecc268c1
Edilson Borges <edilsonfb@gmail.com>
2017-07-24 23:53:42 -0300

    tipos primitivos inteiros

--- CursoJava/src/fundamentos/TiposPrimitivos.java
+++ CursoJava/src/fundamentos/TiposPrimitivos.java
@@ -13,5 +13,12 @@ public class TiposPrimitivos {
 		char c2 = '!';
 		char c3 = '\u0050';
 		System.out.printf("%c %c %c \n", c1, c2, c3);
+		
+		// Tipos primitivos inteiros
+		byte b = 127;
+		short s = 32767;
+		int i = 2_147_483_647;
+		long l = 9_223_337_036_854_775_807L;
+		System.out.printf("%d %d %d %d", b, s, i, l);
 	}
 }
