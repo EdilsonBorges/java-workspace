2c28d6d7d11ff19213181846670acbfdf6d6df21
Edilson Borges <edilsonfb@gmail.com>
2017-07-25 23:15:29 -0300

    Ex2 de conversao explicita, cast para inteiro

--- CursoJava/src/fundamentos/ConversaoNumerica.java
+++ CursoJava/src/fundamentos/ConversaoNumerica.java
@@ -6,5 +6,9 @@ public class ConversaoNumerica {
 		// Ex1 - conversão explícita
 		float f = (float) 0.1;
 		System.out.println(f);
+		
+		// Ex2 - conversão explícita
+		int i1 = (int) 7.9;
+		System.out.println(i1);
 	}
 }
