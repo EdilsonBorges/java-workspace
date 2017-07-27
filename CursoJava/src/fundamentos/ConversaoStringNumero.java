f3981bd6855b18173c6eee8628a9c4331c65497b
Edilson Borges <edilsonfb@gmail.com>
2017-07-26 23:49:28 -0300

    Concatenação de resultados

--- CursoJava/src/fundamentos/ConversaoStringNumero.java
+++ CursoJava/src/fundamentos/ConversaoStringNumero.java
@@ -5,7 +5,9 @@ import javax.swing.JOptionPane;
 public class ConversaoStringNumero {
 
 	public static void main(String[] args){
-		String resposta = JOptionPane.showInputDialog("Qual é a nota?");
-		System.out.println(resposta);
+		String resposta1 = JOptionPane.showInputDialog("Qual é a nota 1?");
+		String resposta2 = JOptionPane.showInputDialog("Qual é a nota 2?");
+		
+		System.out.println(resposta1 + resposta2);
 	}
 }
