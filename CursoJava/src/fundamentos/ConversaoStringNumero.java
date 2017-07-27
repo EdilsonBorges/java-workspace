63f1238dc3c4a04915d975ddeb10009b45ee1c29
Edilson Borges <edilsonfb@gmail.com>
2017-07-26 23:47:57 -0300

    Swing JOptionPane

--- CursoJava/src/fundamentos/ConversaoStringNumero.java
+++ CursoJava/src/fundamentos/ConversaoStringNumero.java
@@ -1,8 +1,11 @@
 package fundamentos;
 
+import javax.swing.JOptionPane;
+
 public class ConversaoStringNumero {
 
 	public static void main(String[] args){
-		
+		String resposta = JOptionPane.showInputDialog("Qual é a nota?");
+		System.out.println(resposta);
 	}
 }
