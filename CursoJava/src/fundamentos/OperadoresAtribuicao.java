ec51633009b3803f9e5d0134ae099cefad11991b
Edilson Borges <edilsonfb@gmail.com>
2017-07-27 21:33:41 -0300

    Operadores de Atribuicao

---
+++ CursoJava/src/fundamentos/OperadoresAtribuicao.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+public class OperadoresAtribuicao {
+
+	public static void main(String[] args) {
+		int a = 7;
+		int b = 3;
+		
+		b += a;
+		System.out.println(b); // 10
+		b *= 4;
+		System.out.println(b); // 40
+	}
+}
