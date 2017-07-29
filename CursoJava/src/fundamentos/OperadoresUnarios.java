521807a5be4fb5b64aa180e9368179f9002fe588
Edilson Borges <edilsonfb@gmail.com>
2017-07-28 23:30:05 -0300

    Operadores Unários

---
+++ CursoJava/src/fundamentos/OperadoresUnarios.java
@@ -0,0 +1,13 @@
+package fundamentos;
+
+public class OperadoresUnarios {
+
+	public static void main(String[] args) {
+		int num1 = -1;
+		int num2 = +2;
+		
+		// Incremento e decremento
+		num1++;
+		System.out.println(num1);
+	}
+}
