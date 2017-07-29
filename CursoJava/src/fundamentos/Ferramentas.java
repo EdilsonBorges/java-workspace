791190f60e2b8647d27567a49dd7673a04144587
Edilson Borges <edilsonfb@gmail.com>
2017-07-29 19:30:49 -0300

    JDK

--- CursoJava/src/fundamentos/Ferramentas.java
+++ CursoJava/src/fundamentos/Ferramentas.java
@@ -3,9 +3,12 @@ package fundamentos;
 public class Ferramentas {
 
 	/*
-	 * Java Runtime Environment (JRE): Ambiente de Tempo de Execução Java, e é utilizado
+	 * JRE (Java Runtime Environment): Ambiente de Tempo de Execução Java, e é utilizado
 	 * 	para executar as aplicações da plataforma Java. É composto por bibliotecas (APIs) e
 	 * 	pela Máquina virtual Java (JVM).
 	 * 
+	 * JDK (Java Development Kit): significa Kit de Desenvolvimento Java, e é um conjunto
+	 * 	de utilitários que permitem criar sistemas de software para a plataforma Java.
+	 * 	É composto por compilador e bibliotecas.
 	 * */
 }
