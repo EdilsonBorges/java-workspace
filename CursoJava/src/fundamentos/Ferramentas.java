ec7c89421fdcd18ddbe4525b22d3508c8eaccd0c
Edilson Borges <edilsonfb@gmail.com>
2017-07-29 19:29:49 -0300

    JRE

---
+++ CursoJava/src/fundamentos/Ferramentas.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class Ferramentas {
+
+	/*
+	 * Java Runtime Environment (JRE): Ambiente de Tempo de Execução Java, e é utilizado
+	 * 	para executar as aplicações da plataforma Java. É composto por bibliotecas (APIs) e
+	 * 	pela Máquina virtual Java (JVM).
+	 * 
+	 * */
+}
