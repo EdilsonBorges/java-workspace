ec91b036d9610e9981c72f9b3e8681d17a279987
Edilson Borges <edilsonfb@gmail.com>
2017-07-28 23:35:19 -0300

    operadores unários com operadores lógicos

--- CursoJava/src/fundamentos/OperadoresUnarios.java
+++ CursoJava/src/fundamentos/OperadoresUnarios.java
@@ -3,13 +3,16 @@ package fundamentos;
 public class OperadoresUnarios {
 
 	public static void main(String[] args) {
-		int num1 = -1;
-		int num2 = +2;
+		int num1 = 1;
+		int num2 = 2;
 		
 		// Incremento e decremento
 		num1++;
 		System.out.println(num1);
 		--num1;
 		System.out.println(num1);
+		
+		System.out.println(++num1 == num2--);
+		System.out.println(num1 == num2);
 	}
 }
