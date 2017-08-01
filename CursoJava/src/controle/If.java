8cc2b97621ec75433e48eb8db2d34af09b23d344
Edilson Borges <edilsonfb@gmail.com>
2017-07-31 23:55:08 -0300

    ; recomendacao

--- CursoJava/src/controle/If.java
+++ CursoJava/src/controle/If.java
@@ -5,8 +5,8 @@ public class If {
 	public static void main(String[] args) {
 		double nota = 9.5;
 		boolean bomComportamento = false;
-		if(nota >= 9 && bomComportamento)
-		;
+		
+		if (nota >= 9 && bomComportamento); // tomar cuidado com o ; pois existe finalização da condição
 		{
 			System.out.println("Quadro de teste");
 		}
