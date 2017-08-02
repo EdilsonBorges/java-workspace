3115de995f75927057bc8300d01702db7dc21e2b
Edilson Borges <edilsonfb@gmail.com>
2017-08-01 21:56:36 -0300

    exemplo de ; interrompendo a condicional

--- CursoJava/src/controle/IfElse.java
+++ CursoJava/src/controle/IfElse.java
@@ -3,10 +3,10 @@ package controle;
 public class IfElse {
 
 	public static void main(String[] args) {
-		double nota = 6.5;
+		double nota = 8.5;
 		if(nota >= 7) {
 			System.out.println("Aprovado!");
-		} else {
+		} else; {
 			System.out.println("Recuperação");
 		}
 	}
