aa9e58eea02c95e45dd4a4bc1941801eed9d8d63
Edilson Borges <edilsonfb@gmail.com>
2017-08-03 23:00:14 -0300

    Do While

---
+++ CursoJava/src/controle/DoWhile.java
@@ -0,0 +1,14 @@
+package controle;
+
+import java.util.Scanner;
+
+public class DoWhile {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		double somaDasNotas = 0;
+		double nota = 0;
+		int numeroDeNotas = 0;
+		
+	}
+}
