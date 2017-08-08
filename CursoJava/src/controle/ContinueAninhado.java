94e0dd976c563df4d24c071b2631e1f1bf22fc33
Edilson Borges <edilsonfb@gmail.com>
2017-08-08 00:04:22 -0300

    Continue rotulado

--- CursoJava/src/controle/ContinueAninhado.java
+++ CursoJava/src/controle/ContinueAninhado.java
@@ -3,10 +3,10 @@ package controle;
 public class ContinueAninhado {
 
 	public static void main(String[] args) {
-		for (int i = 0; i < 5; i++) {
+		externo: for (int i = 0; i < 5; i++) {
 			for (int j = 0; j < 5; j++){
 				if(j == 2) {
-					continue;
+					continue externo; // continue e break rotulados não são boas práticas de programação
 				}
 				System.out.println(i + " " + j);	
 			}
