fc9797587519587e9cced0a204f0781e10cd3b4a
Edilson Borges <edilsonfb@gmail.com>
2017-08-08 23:34:50 -0300

    criação de método na classe Data e acesso à ele na classe DataTeste

--- CursoJava/src/classe/DataTeste.java
+++ CursoJava/src/classe/DataTeste.java
@@ -8,7 +8,6 @@ public class DataTeste {
 		nascimento.mes = 11;
 		nascimento.ano = 1987;
 		
-		System.out.println(nascimento.dia + "/" + nascimento.mes + "/" + nascimento.ano);
-		System.out.printf("%d/%d/%d", nascimento.dia, nascimento.mes, nascimento.ano);
+		System.out.println(nascimento.formatar());
 	}
 }
