69f681783f47e79d3728397f5af5482a571ba562
Edilson Borges <edilsonfb@gmail.com>
2017-08-09 19:54:58 -0300

    corrigindo erro da classe DataTeste

--- CursoJava/src/classe/DataTeste.java
+++ CursoJava/src/classe/DataTeste.java
@@ -3,10 +3,10 @@ package classe;
 public class DataTeste {
 
 	public static void main(String[] args) {
-		Data nascimento = new Data();
-		nascimento.dia = 19;
-		nascimento.mes = 11;
-		nascimento.ano = 1987;
+		Data nascimento = new Data(19, 11, 1987);
+//		nascimento.dia = 19;
+//		nascimento.mes = 11;
+//		nascimento.ano = 1987;
 		
 		System.out.println(nascimento.formatar());
 	}
