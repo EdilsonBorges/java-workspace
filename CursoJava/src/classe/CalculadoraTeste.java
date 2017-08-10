abf6771460cb307b14ba2c9f860387b33703f784
Edilson Borges <edilsonfb@gmail.com>
2017-08-10 19:30:34 -0300

    acrescentando e obtendo resultados

--- CursoJava/src/classe/CalculadoraTeste.java
+++ CursoJava/src/classe/CalculadoraTeste.java
@@ -6,5 +6,8 @@ public class CalculadoraTeste {
 		Calculadora calc = new Calculadora();
 		
 		System.out.println(calc.somar(2.3, 4.5));
+		
+		calc.acrescentar(10);
+		System.out.println(calc.obterResultado());
 	}
 }
