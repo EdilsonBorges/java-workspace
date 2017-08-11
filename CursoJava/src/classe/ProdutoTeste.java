0b2f713ec0c6ba25633122ae211df97de4d84661
Edilson Borges <edilsonfb@gmail.com>
2017-08-11 16:58:42 -0300

    2 produtos e chamada para preço com desconto

--- CursoJava/src/classe/ProdutoTeste.java
+++ CursoJava/src/classe/ProdutoTeste.java
@@ -3,6 +3,14 @@ package classe;
 public class ProdutoTeste {
 
 	public static void main(String[] args) {
+		Produto produto1 = new Produto();
+		produto1.nome = "Notebook Acter 15' 8Gb";
+		produto1.preco = 2300;
+		produto1.desconto = 0.15;
 		
+		Produto produto2 = new Produto("Caneta BIC Preta", 2.83, 0.05);
+		
+		System.out.println(produto1.precoComDesconto());
+		System.out.println(produto2.precoComDesconto());
 	}
 }
