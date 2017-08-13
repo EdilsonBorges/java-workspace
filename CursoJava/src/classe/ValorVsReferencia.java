be15a186ae2bb8186ac598983c34261a055c9c5e
Edilson Borges <edilsonfb@gmail.com>
2017-08-13 16:40:24 -0300

    método main e utilizacao de métodos de exemplo

--- CursoJava/src/classe/ValorVsReferencia.java
+++ CursoJava/src/classe/ValorVsReferencia.java
@@ -17,4 +17,15 @@ public class ValorVsReferencia {
 	static void porReferencia(Produto produto) {
 		produto.preco++;
 	}
+
+	public static void main(String[] args) {
+		double numero = 1;
+		Produto produto = new Produto("Caneta", 1);
+		
+		porValor(numero);
+		porReferencia(produto);
+		
+		System.out.println(numero);
+		System.out.println(produto.preco);
+	}
 }
