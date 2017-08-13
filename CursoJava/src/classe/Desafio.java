ac2e3b5fbdc3fe462a81ed8de991762611a20b0c
Edilson Borges <edilsonfb@gmail.com>
2017-08-12 22:13:20 -0300

    Solução apresentada e explicação

--- CursoJava/src/classe/Desafio.java
+++ CursoJava/src/classe/Desafio.java
@@ -3,9 +3,15 @@ package classe;
 public class Desafio {
 
 	int a = 2;
-	
-	public static void main(String[] args) {
-		int a = new Desafio().a;
+
+	public static void main(String[] args) { // não é um método de instância, mas um método que pertence à classe (que vem antes da instância)
+//		int a = new Desafio().a;
+//		System.out.println(a);
+		Desafio d = new Desafio(); // Solução apresentada
+		System.out.println(d.a);
+	}
+
+	void teste() { // teste() é um método de instância, por isso ele consegue acessar as variáveis de instância
 		System.out.println(a);
 	}
 }
