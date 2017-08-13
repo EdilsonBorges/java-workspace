5f519cad06f7bf58c07cdf5f0232b9785a41d699
Edilson Borges <edilsonfb@gmail.com>
2017-08-13 16:34:50 -0300

    exemplo - passagem valor vs referencia

--- CursoJava/src/classe/ValorVsReferencia.java
+++ CursoJava/src/classe/ValorVsReferencia.java
@@ -1,11 +1,20 @@
 package classe;
 
 public class ValorVsReferencia {
-	int a = 2;
-	int b = a; // passagem por valor
-	int c; // valor 0 na memória
-	
-	Object a = new Object();
-	Object b = a; // passagem por referência
-	Object c; // null
+
+	// int a = 2;
+	// int b = a; // passagem por valor
+	// int c; // valor 0 na memória
+	//
+	// Object a = new Object();
+	// Object b = a; // passagem por referência
+	// Object c; // null
+
+	static void porValor(double numero) {
+		numero++;
+	}
+
+	static void porReferencia(Produto produto) {
+		produto.preco++;
+	}
 }
