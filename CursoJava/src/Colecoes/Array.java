15462bbb7a478ae7093f6e635c2c9ac9ff0ecc57
Edilson Borges <edilsonfb@gmail.com>
2017-08-15 20:37:40 -0300

    Outra maneira de criar um array no java

--- CursoJava/src/Colecoes/Array.java
+++ CursoJava/src/Colecoes/Array.java
@@ -4,5 +4,13 @@ public class Array {
 
 	public static void main(String[] args) {
 		double[] notasJoao = {7.0, 8.7, 6.2, 4.9, 9.7};
+		
+		double[] notasPedro = new double[5]; //Array tem tamanho fixo
+		notasPedro[0] = 7.0;
+		notasPedro[1] = 8.7;
+		notasPedro[2] = 6.2;
+		notasPedro[3] = 4.9;
+		notasPedro[4] = 9.7;
 	}
 }
+
