17c9c16e47bd23aed7ea0be7958513d26d21bb6e
Edilson Borges <edilsonfb@gmail.com>
2017-08-15 22:29:49 -0300

    Array multidimensional elemento por elemento

--- CursoJava/src/colecoes/Matriz.java
+++ CursoJava/src/colecoes/Matriz.java
@@ -4,5 +4,22 @@ public class Matriz {
 
 	public static void main(String[] args) {
 		double[][] notasJoao = { { 7.7, 8.6 }, { 6.2, 9.7 } };
+		
+		double[][] notasPedro = new double[2][];
+		notasPedro[0] = new double[2];
+		notasPedro[1] = new double[2];
+		notasPedro[0][0] = 7.7;
+		notasPedro[0][1] = 8.6;
+		notasPedro[1][0] = 6.2;
+		notasPedro[1][1] = 9.7;
+		
+		double totalJoao = 0;
+		int qtdeNotasJoao = 0;
+		for (int i = 0; i < notasPedro.length; i++) {
+			for (int j = 0; j < notasPedro.length; j++) {
+				totalJoao += notasJoao[i][j];
+				qtdeNotasJoao++;
+			}
+		}
 	}
 }
