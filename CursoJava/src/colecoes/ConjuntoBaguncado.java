46692c4fff3f85289895f4d1b0243b19de552c87
Edilson Borges <edilsonfb@gmail.com>
2017-08-16 23:20:18 -0300

    Adicionando varios tipos de elementos no conjunto

--- CursoJava/src/colecoes/ConjuntoBaguncado.java
+++ CursoJava/src/colecoes/ConjuntoBaguncado.java
@@ -7,5 +7,10 @@ public class ConjuntoBaguncado {
 
 	public static void main(String[] args) {
 		Set conjunto = new HashSet();
+		conjunto.add('a');
+		conjunto.add("Texto");
+		conjunto.add(1);
+		conjunto.add(3.14);
+		conjunto.add(false);
 	}
 }
