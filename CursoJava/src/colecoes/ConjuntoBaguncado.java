e9fade7e2fe04f560b5667d21de87ffba5605f7c
Edilson Borges <edilsonfb@gmail.com>
2017-08-16 23:08:32 -0300

    Colecoes - Set

---
+++ CursoJava/src/colecoes/ConjuntoBaguncado.java
@@ -0,0 +1,11 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class ConjuntoBaguncado {
+
+	public static void main(String[] args) {
+		Set conjunto = new HashSet();
+	}
+}
