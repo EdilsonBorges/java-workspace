1801f27ddeb944b8a49bd535e2ec6f6f950e0ecc
Edilson Borges <edilsonfb@gmail.com>
2017-08-18 17:50:08 -0300

    Adicionando registros e printanto na tela

--- CursoJava/src/colecoes/ConjuntoComportado.java
+++ CursoJava/src/colecoes/ConjuntoComportado.java
@@ -7,5 +7,13 @@ public class ConjuntoComportado {
 
 	public static void main(String[] args) {
 		Set<String> aprovados = new HashSet();
+		aprovados.add("João");
+		aprovados.add("Pedro");
+		aprovados.add("Maria");
+		aprovados.add("Rafael");
+		
+		for(Object nome: aprovados){
+			System.out.println(nome);
+		}
 	}
 }
