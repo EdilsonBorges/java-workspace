edf1b2076b4e4348c7488b678e29b70143de3a1d
Edilson Borges <edilsonfb@gmail.com>
2017-08-18 17:59:10 -0300

    Utilizando linkedHashSet para ordenação por inserção

--- CursoJava/src/colecoes/ConjuntoComportado.java
+++ CursoJava/src/colecoes/ConjuntoComportado.java
@@ -1,19 +1,22 @@
 package colecoes;
 
-import java.util.HashSet;
+import java.util.LinkedHashSet;
 import java.util.Set;
 
 public class ConjuntoComportado {
 
 	public static void main(String[] args) {
-		Set<String> aprovados = new HashSet();
+		Set<String> aprovados = new LinkedHashSet<>();
 		aprovados.add("João");
 		aprovados.add("Pedro");
 		aprovados.add("Maria");
 		aprovados.add("Rafael");
 		
-		for(Object nome: aprovados){
+		for(/* Object substituído por */String nome: aprovados){
 			System.out.println(nome);
+					// HashSet: João Rafael Pedro Maria - sem ordem nenhuma
+					// LinkedHashSet: João Pedro Maria Rafael - com ordem de inserção
+
 		}
 	}
 }
