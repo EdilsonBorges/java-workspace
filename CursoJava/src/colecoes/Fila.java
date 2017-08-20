2e93022b5aebfeca309d0ebb801449f2b9859a24
Edilson Borges <edilsonfb@gmail.com>
2017-08-19 23:47:11 -0300

    Offer e Add

--- CursoJava/src/colecoes/Fila.java
+++ CursoJava/src/colecoes/Fila.java
@@ -7,5 +7,9 @@ public class Fila {
 
 	public static void main(String[] args) {
 		Queue<String> fila = new LinkedList<>();
+		
+		fila.offer("Maria"); // caso não consiga adicionar, o retorno é um boolean false
+		fila.offer("João");
+		fila.add("Pedro"); // caso não consiga adicionar, o retorno é um erro
 	}
 }
