61e4b43a65307f9570ae4f088537b8f173174e99
Edilson Borges <edilsonfb@gmail.com>
2017-08-22 17:46:03 -0300

    Método equals

--- CursoJava/src/colecoes/EnderecoTeste.java
+++ CursoJava/src/colecoes/EnderecoTeste.java
@@ -5,6 +5,8 @@ public class EnderecoTeste {
 	public static void main(String[] args) {
 		Endereco endereco1 = new Endereco("Rua Loop Infinito", 1, "Sala 1001");
 		Endereco endereco2 = new Endereco("Rua Loop Infinito", 1, "Sala 1001");
-		System.out.println(endereco1 == endereco2); // falso, comparação de endereço de memória
+		System.out.println(endereco1 == endereco2); // false, comparação de endereço de memória
+		System.out.println(endereco1.equals(endereco2)); // false, está usando o método equal da classe Object
+		
 	}
 }
