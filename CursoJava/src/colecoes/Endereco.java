adba7e66dba1956c435a445b48eb2ef6d6e3ec2e
Edilson Borges <edilsonfb@gmail.com>
2017-08-22 17:32:20 -0300

    Construtor para endereço

--- CursoJava/src/colecoes/Endereco.java
+++ CursoJava/src/colecoes/Endereco.java
@@ -5,4 +5,10 @@ public class Endereco {
 	String logradouro;
 	int numero;
 	String complemento;
+	
+	Endereco(String logradouro, int numero, String complemento){
+		this.logradouro = logradouro;
+		this.numero = numero;
+		this.complemento = complemento;
+	}
 }
