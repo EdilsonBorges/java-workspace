0930e85b41a2ec33f9db76c52883caac1f9178f4
Edilson Borges <edilsonfb@gmail.com>
2017-08-23 20:38:45 -0300

    Átributo protected

--- CursoJava/src/OO/encapsulamento/casa2/Genro.java
+++ CursoJava/src/OO/encapsulamento/casa2/Genro.java
@@ -1,5 +1,6 @@
 package OO.encapsulamento.casa2;
 
+import OO.encapsulamento.casa1.Sogra;
 import OO.encapsulamento.casa1.Sogro;
 
 public class Genro {
@@ -14,6 +15,9 @@ public class Genro {
 		System.out.println(meuAmor.segredoDoCasal); // Variável default pode ser acessada estando no mesmo pacote
 		
 		Sogro sograo = new Sogro();
-		System.out.println(sograo.gostaDeCerveja);
+		System.out.println(sograo.gostaDeCerveja); // Variável pública pode ser acessada por todos
+		
+		Sogra sogrinha = new Sogra();
+		System.out.println(sogrinha.segredoDeFamilia); // Variável protected só consegue ser acessada se estiver no mesmo pacote ou se recebesse essa informação  por herança
 	}
 }
