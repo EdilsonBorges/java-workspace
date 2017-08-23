aaf9c1e37bbd1df6996ab4c78971d9bb597b6c6f
Edilson Borges <edilsonfb@gmail.com>
2017-08-23 20:45:50 -0300

    Acessando variável herdada

--- CursoJava/src/OO/encapsulamento/casa2/Filha.java
+++ CursoJava/src/OO/encapsulamento/casa2/Filha.java
@@ -1,6 +1,7 @@
 package OO.encapsulamento.casa2;
 
 import OO.encapsulamento.casa1.Sogra;
+import OO.encapsulamento.casa1.Sogro;
 
 public class Filha extends Sogra {
 
@@ -9,5 +10,16 @@ public class Filha extends Sogra {
 	public static void main(String[] args) {
 		Filha euMesma = new Filha();
 		System.out.println("Meus pais não sabem... " + euMesma.segredoDoCasal);
+		
+//		Genro meuAmor = new Genro();
+//		System.out.println(meuAmor.segredo); // Atributo de genro é private, não pode ser acessado por ninguém além da própria classe
+		
+		Sogro papai = new Sogro();
+		System.out.println(papai.gostaDeCerveja);
+		
+//		Sogra mamae = new Sogra();
+//		System.out.println(mamae.segredoDeFamilia); // Atributo protected não consegue ser acessado, pois foi herdado para a própria classe
+		
+		System.out.println(euMesma.segredoDeFamilia);
 	}
 }
