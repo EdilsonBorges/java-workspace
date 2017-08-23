2a226d6494b2ae698b9f9900d86d6b3d6a521ff5
Edilson Borges <edilsonfb@gmail.com>
2017-08-23 20:31:46 -0300

    Acessando o prório atribuito da classe, private

--- CursoJava/src/OO/encapsulamento/casa2/Genro.java
+++ CursoJava/src/OO/encapsulamento/casa2/Genro.java
@@ -3,4 +3,9 @@ package OO.encapsulamento.casa2;
 public class Genro {
 
 	private String segredo = "Minha sogra fala demais!";
+	
+	public static void main(String[] args) {
+		Genro euMesmo = new Genro();
+		System.out.println("Nunca falarei ... " + euMesmo.segredo);
+	}
 }
