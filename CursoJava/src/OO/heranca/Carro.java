f68ef8bfd93adfdbb102e30a76f4c0de67094f7b
Edilson Borges <edilsonfb@gmail.com>
2017-08-24 11:13:38 -0300

    Método final acelerarMais

--- CursoJava/src/OO/heranca/Carro.java
+++ CursoJava/src/OO/heranca/Carro.java
@@ -4,4 +4,12 @@ public class Carro {
 
 	protected int VELOCIDADE_MAXIMA = 200; // na prática o atributo seria uma constante
 	private int velocidadeAtual = 0;
+	
+	protected final void acelerarMais(int velocidade){ // método final não pode ser modificado pelas classes filhas 
+		if(velocidadeAtual + velocidade > VELOCIDADE_MAXIMA){
+			velocidadeAtual = VELOCIDADE_MAXIMA;
+		} else {
+			velocidadeAtual += velocidade;
+		}
+	}
 }
