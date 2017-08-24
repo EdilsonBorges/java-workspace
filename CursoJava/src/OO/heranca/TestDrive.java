0cabb7b236f80862c8404e3b4e024395c3f4017e
Edilson Borges <edilsonfb@gmail.com>
2017-08-24 11:28:50 -0300

    Método main e implementação de exemplos

--- CursoJava/src/OO/heranca/TestDrive.java
+++ CursoJava/src/OO/heranca/TestDrive.java
@@ -3,6 +3,12 @@ package OO.heranca;
 public class TestDrive {
 
 	public static void main(String[] args) {
-		
+		Carro c = new Civic();
+		c.acelerar();
+		System.out.println(c.getVelocidadeAtual());
+
+		c = new Ferrari();
+		c.acelerar();
+		System.out.println(c.getVelocidadeAtual());
 	}
 }
