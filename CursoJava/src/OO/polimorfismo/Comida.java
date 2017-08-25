930636a7a5ac9078801f37a629715be45aefec5d
Edilson Borges <edilsonfb@gmail.com>
2017-08-25 11:52:38 -0300

    Métodos assessores

--- CursoJava/src/OO/polimorfismo/Comida.java
+++ CursoJava/src/OO/polimorfismo/Comida.java
@@ -5,5 +5,15 @@ public class Comida {
 	/**
 	 * Peso em kg
 	 */
-	private double peso = 0;	
+	private double peso = 0;
+
+	public double getPeso() { // método assessor get
+		return peso;
+	}
+
+	public void setPeso(double peso) { // método assessor set
+		if (peso > 0) {
+			this.peso = peso;
+		}
+	}
 }
