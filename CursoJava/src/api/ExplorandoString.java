d42474d6757ff8bb59ef2d2c68e93e861c6cd048
Edilson Borges <edilsonfb@gmail.com>
2017-08-27 16:52:22 -0300

    Métodos para string

--- CursoJava/src/api/ExplorandoString.java
+++ CursoJava/src/api/ExplorandoString.java
@@ -4,5 +4,14 @@ public class ExplorandoString {
 
 	public static void main(String[] args) {
 		String nome = new String("Leonardo");
+		nome = "Leo" + "nardo";
+		
+		// Alguns métodos
+		System.out.println(nome.toUpperCase());
+		System.out.println(nome.length());
+		System.out.println(nome.substring(0, 3));
+		System.out.println(nome.indexOf('d'));
+		System.out.println(nome.charAt(6));
+		System.out.println(nome.equalsIgnoreCase("leonardo"));
 	}
 }
