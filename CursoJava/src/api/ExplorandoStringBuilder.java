8dd80d7778e6cf2ea46a98d6d488cc36890beb1d
Edilson Borges <edilsonfb@gmail.com>
2017-08-28 19:25:14 -0300

    Append no objeto StringBuilder sb

--- CursoJava/src/api/ExplorandoStringBuilder.java
+++ CursoJava/src/api/ExplorandoStringBuilder.java
@@ -3,6 +3,9 @@ package api;
 public class ExplorandoStringBuilder {
 
 	public static void main(String[] args) {
-		
+		StringBuilder sb = new StringBuilder();
+		sb.append("João");
+		sb.append(",").append("Maria");
+		sb.append(",").append("Pedro");
 	}
 }
