6ebfd19cd5e59d4991117124c173dcfd07816638
Edilson Borges <edilsonfb@gmail.com>
2017-08-29 18:43:35 -0300

    Método abs, min

--- CursoJava/src/api/ExplorandoMath.java
+++ CursoJava/src/api/ExplorandoMath.java
@@ -5,5 +5,7 @@ public class ExplorandoMath {
 	public static void main(String[] args) {
 		System.out.println(Math.PI); // Vários métodos estáticos, não precisa de instanciar
 		System.out.println(Math.pow(2,  3));
+		System.out.println(Math.abs(-10.5));
+		System.out.println(Math.min(70, 7));
 	}
 }
