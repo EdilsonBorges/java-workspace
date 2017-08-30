ea56090b3716cfae87dfa521066fd1225f61e09a
Edilson Borges <edilsonfb@gmail.com>
2017-08-30 19:39:23 -0300

    fatorialFor

--- CursoJava/src/avancado/Recursividade.java
+++ CursoJava/src/avancado/Recursividade.java
@@ -3,6 +3,14 @@ package avancado;
 public class Recursividade {
 
 	public static void main(String[] args) {
-		
+
+	}
+
+	public static int fatorialFor(int numero) {
+		int fatorial = 1;
+		for (int i = 1; i <= numero; i++) {
+			fatorial = fatorial * i;
+		}
+		return fatorial;
 	}
 }
