ed35fe0710864d949f9e36d5b2f9331cb7a2aa6e
Edilson Borges <edilsonfb@gmail.com>
2017-09-01 14:58:05 -0300

    Javadoc

---
+++ CursoJava/src/api/Conceitos.java
@@ -0,0 +1,17 @@
+package api;
+
+public class Conceitos {
+/*
+ * Modificadores de Acesso: private, protected, public
+ * Modificadores:			abstract, class, extends, final, implements, interface, native, new, static, strictfp, synchronized, transient, volatile
+ * Operador:				instanceof
+ * Estruturas de Controla:	case, continue, default, do, else, for, if, break, return, switch, while
+ * Tratamento de Erro:		assert, catch, finally, throw, throws, try
+ * Controle de Pacotes:		import
+ * Tipos Primitivos:		boolean, byte, short, int, long, float, double, char
+ * Variáveis de Referência:	super, this
+ * Retorno de Método:		void
+ * Não Utilizadas:			const, goto
+ * Literais Reservados:		null, true, false
+ */
+}
