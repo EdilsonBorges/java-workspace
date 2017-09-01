90c2094d586cbcf2705235f71709d32420ae4558
Edilson Borges <edilsonfb@gmail.com>
2017-08-31 22:11:35 -0300

    Importando acm.jar, exemplo Sierpinski

---
+++ CursoJava/src/avancado/Sierpinski.java
@@ -0,0 +1,35 @@
+package avancado;
+
+import acm.graphics.GRect;
+import acm.program.GraphicsProgram;
+import java.awt.Color;
+
+public class Sierpinski extends GraphicsProgram {
+
+    public void run() {
+        GRect box = new GRect(20, 20, 242, 242);
+        box.setFilled(true);
+        add(box);
+        drawGasket(20, 20, 243);
+    }
+
+    private void drawGasket(int x, int y, int side) {
+        int sub = side / 3;
+        GRect box = new GRect(x + sub, y + sub, sub - 1, sub - 1);
+        box.setFilled(true);
+        box.setColor(Color.WHITE);
+        add(box);
+
+        if (sub >= 3) {
+            drawGasket(x, y, sub);
+            drawGasket(x + sub, y, sub);
+            drawGasket(x + 2 * sub, y, sub);
+            drawGasket(x, y + sub, sub);
+            drawGasket(x + 2 * sub, y + sub, sub);
+            drawGasket(x, y + 2 * sub, sub);
+            drawGasket(x + sub, y + 2 * sub, sub);
+            drawGasket(x + 2 * sub, y + 2 * sub, sub);
+        }
+    }
+
+}
